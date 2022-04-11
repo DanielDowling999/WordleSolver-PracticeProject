2407244023aea077a3ca6b1313677b9630136399
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2022-04-11 17:25:46 +1200

    Added initial version of wordle solver project
    Currently, system has hardcoded user inputs, no gui, and does not allow . It is able to output a list of words that are possible, given the hardcoded user inputs.

---
+++ wordleSolver.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+//import java.util.Scanner;
+
+
+public class wordleSolver {
+
+	public void solveWordle(){	
+		
+	char[] correctChars = {'a', 'b', 'y'};
+	char[] incorrectChars = {'g', 'w', 'l', 'k'};
+	Set<String> answers = findWords(correctChars, incorrectChars);
+	if(answers.size() == 0){
+		System.out.println("Invalid combination of letters");
+	}
+	else{
+		System.out.println("Possible answers include: ");
+		for(String answer:answers){
+			System.out.print(answer + ", ");
+		}
+	}
+
+
+	}
+
+	public Set<String> findWords(char[] correctChars, char[] incorrectChars){
+		File file = new File("words.txt");
+		Set<String> words = new HashSet<String>();
+		Boolean escape = false;
+
+		try{
+			Scanner sc = new Scanner(file);
+			while(sc.hasNextLine()){
+				String word = sc.next();
+				for(int i = 0; i<incorrectChars.length; i++){
+					if(word.indexOf(incorrectChars[i]) == -1){
+						
+					}
+					else{
+						escape = true;
+						break;
+					}
+				}
+				if(escape){
+					escape = false;
+					//continue;
+				}
+				else{
+					for(int i =0; i<correctChars.length; i++){
+						if(word.indexOf(correctChars[i]) == -1){
+							escape = true;
+							break;
+						}
+					}
+					if(escape){
+						escape = false;
+					}
+					else{
+						words.add(word);
+
+					}
+				}
+			}
+			return words;
+	
+
+		}catch(IOException E){
+			return words;
+		}
+		
+	}
+}
