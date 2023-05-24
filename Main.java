3b126515bba5707ad78386687e24f872d7966354
DanielDowling999 <ddow731@aucklanduni.ac.nz>
2023-05-24 19:12:01 +1200

    Finally added ui and user input elements, completing the main functionality

--- Main.java
+++ Main.java
@@ -1,6 +1,72 @@
-public class Main {
+import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class Main implements ActionListener{
+  private String wrngLetters;
+  private String corrLetters;
+  private JFrame frame;
+  private JPanel panel;
+  private JLabel correctLabel;
+  private JLabel wrongLabel;
+  private JTextField correctLetters;
+  private JTextField wrongLetters;
+  private JLabel answersLabel;
+  private Set<String> answers;
+  public Main() {
+    frame = new JFrame();
+    panel = new JPanel();
+    correctLabel = new JLabel("Correct Letters: ");
+    wrongLabel = new JLabel("Wrong Letters: ");
+    correctLetters = new JTextField();
+    wrongLetters = new JTextField(); 
+    GridLayout appLayout = new GridLayout(0,2);
+    JButton solverButton = new JButton("Solve!"); 
+    answersLabel = new JLabel(" Possible Answers: Null");
+    panel.setBorder(BorderFactory.createEmptyBorder(10,30,10,30));
+    panel.setLayout(appLayout);
+    panel.add(correctLabel);
+    panel.add(correctLetters);
+    panel.add(wrongLabel);
+    panel.add(wrongLetters);
+    
+    panel.add(solverButton);
+    panel.add(answersLabel);
+    solverButton.addActionListener(this);
+    frame.add(panel, BorderLayout.CENTER);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setTitle("Wordle Solver");
+    frame.pack();
+    frame.setVisible(true);
+
+  }
     public static void main(String[] args) {
-		wordleSolver obj = new wordleSolver();
-        obj.solveWordle();
+      new Main();
+		//wordleSolver obj = new wordleSolver();
+      //  obj.solveWordle();
 	}
+    @Override //Finds any valid 5 letter words from a pre-set dictionary that match the correct letters and incorrect letters entered.
+    //Returns an error message if the response returns no valid characters.
+    //Updates the Answer Label to contain the valid words. Requires the user to extend their window size if they want to see them all.
+    public void actionPerformed(ActionEvent e) {
+      int count = 0;
+      corrLetters = correctLetters.getText();
+      wrngLetters = wrongLetters.getText();
+      wordleSolver obj = new wordleSolver();
+      answers = obj.solveWordle(corrLetters, wrngLetters);
+      String outString = "<html> Possible Answers:<br> ";
+      for(String answer:answers){
+        outString = outString + answer + ", ";
+        count++;
+        if (count>5){
+          count = 0;
+          outString = outString + "<br> ";
+        }
+      }
+      answersLabel.setText(outString);
+    }
 }
--- wordleSolver.java
+++ wordleSolver.java
@@ -5,13 +5,19 @@ import java.util.*;
 
 public class wordleSolver {
 
-	public void solveWordle(){	
-		
-	char[] correctChars = {'a', 'b', 'z'};
-	char[] incorrectChars = {'g', 'w', 'l', 'k'};
+	public Set<String> solveWordle(String correctLetters, String wrongLetters){	
+	correctLetters = correctLetters.toLowerCase();
+	wrongLetters = wrongLetters.toLowerCase();	
+	char[] correctChars = correctLetters.toCharArray();
+	char[] incorrectChars = wrongLetters.toCharArray();
 	Set<String> answers = findWords(correctChars, incorrectChars);
+    /*for (char lets:correctChars){
+		System.out.print(lets);
+	}*/
 	if(answers.size() == 0){
 		System.out.println("Invalid combination of letters");
+		answers = new HashSet<String>();
+		answers.add("Invalid Combination of Letters");
 	}
 	else{
 		System.out.println("Possible answers include: ");
@@ -19,15 +25,15 @@ public class wordleSolver {
 			System.out.print(answer + ", ");
 		}
 	}
+	return answers;
 
 
 	}
-
+    //Scans dictionary (a text file that comes with the application) for any valid words, storing them in a Set if found.
 	public Set<String> findWords(char[] correctChars, char[] incorrectChars){
 		File file = new File("words.txt");
 		Set<String> words = new HashSet<String>();
 		Boolean escape = false;
-
 		try{
 			Scanner sc = new Scanner(file);
 			while(sc.hasNextLine()){
@@ -61,6 +67,7 @@ public class wordleSolver {
 					}
 				}
 			}
+			sc.close();
 			return words;
 	
 
